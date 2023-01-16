d401c26b440e3bdac67f519997f8a44eefca5cd3
GustavoFFLima <frazao39@gmail.com>
2023-01-16 12:46:26 -0300

    correções

--- src/app.js
+++ src/app.js
@@ -1,19 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import { MongoClient, ObjectId } from 'mongodb'
-import dotnet from 'dotenv'
-import dayjs from 'daysj'
+import dotenv from 'dotenv'
 
 const app = express()
-const mongoClient = new MongoClient(process.env.DATABASE_URL)
-const PORT = 5000
-let db = mongoClient.db()
-const usuarios = db.collection('participants')
-const mensagens = db.collection('messages')
-
 dotenv.config()
 app.use(express.json())
 app.use(cors())
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+const PORT = 5000
+let db;
 
 app.listen(PORT, () => console.log(`Este servidor esta em execução na porta: ${PORT}`))
 
@@ -23,19 +20,23 @@ try {
     console.log(erro)
 } 
 
+db = mongoClient.db();
+
+const usuarios = db.collection('participants')
+const mensagens = db.collection('messages')
+
 app.post("/participants", async (req,res) => {
     const dados = req.body
     console.log(dados)
     res.send('ok')
     try {
-        const usuario = await usuarios.findOne({dados})
-        const time = dayjs().format('HH:mm:ss')
+        const usuario = await db.collection(usuarios).findOne({dados})
         const mensagem = {from: dados, to:'Todos', text: 'entra na sala...', type: 'status', time}
 
-        if(usuario) return res.sendStatus(400)
-        await usuarios.insertOne({dados, lastStatus: Date.now() })
+        if(usuario) return res.sendStatus(422)
+        await db.collection(usuarios).insertOne({dados, lastStatus: Date.now() })
         mensagens.insertOne({ ...mensagem })
-        res.sendStatus(200)
+        res.sendStatus(201)
     } catch (erro) {
         res.status(500).send(erro)
     }
